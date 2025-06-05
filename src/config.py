f0fa3418b09729e2e1c8cc439c5d8b7e32950263
leooojr <leo.jourdain@aphp.fr>
2025-06-05 18:36:26 +0200

    [UPDATE] worked on cerberus schema validation

--- src/config.py
+++ src/config.py
@@ -1,18 +1,66 @@
 import cerberus
 import jinja2
+from loguru import logger
 import os
 import yaml
 
 class ConfigParser():
 
     validator = cerberus.Validator({"caller": {"type": "dict",
-                                                "schema": {"name": {"type": "string"}, 
-                                                           "format": {"type": "list", 
-                                                                      "schema": {"type": "string"}}, 
-                                                            "VAF": {}, 
-                                                            "depth": {}, 
-                                                            "RRC": {}, 
-                                                            "ARC": {}
+                                                "schema": {"name": {"type": "string",
+                                                                    "empty": False,
+                                                                    "required": True,
+                                                                    "forbidden": ["BCFTools", "Varscan", "Vardict", "Pindel", "Haplotypecaller", "Filt3r", "DeepVariant"]},
+                                                           "info": {"type": "string",
+                                                                    "empty": False,
+                                                                    "required": False,
+                                                                    "nullable": True,
+                                                                    "default": None}, 
+                                                           "format": {"type": "string",
+                                                                      "empty": False,
+                                                                      "required": True,
+                                                                      "regex": "^([A-Za-z]+:[A-Za-z]+)+$",
+                                                                      "coerce": lambda x: x.replace(':',',')}, 
+                                                           "vaf": {"type": "dict",
+                                                                    "schema": {
+                                                                        "extract": {"type": "string",
+                                                                                    "empty": False}
+                                                                    },
+                                                                    "required": True,
+                                                                    "empty": False,
+                                                                    "dependencies": "format"}, 
+                                                            "depth": {"type": "dict",
+                                                                      "schema": {
+                                                                        "extract": {"type": "string",
+                                                                                    "empty": False}
+                                                                    },
+                                                                    "required": True,
+                                                                    "empty": False,
+                                                                    "dependencies": "format"}, 
+                                                            "rrc": {"type": "dict",
+                                                                    "schema": {
+                                                                        "plus": {"type": "string",
+                                                                                 "required": False,
+                                                                                 "empty": False,
+                                                                                 "nullable": True,
+                                                                                 "default": None},
+                                                                        "minus": {"type": "string",
+                                                                                  "required": False,
+                                                                                  "empty": False,
+                                                                                  "nullable": True,
+                                                                                  "default": None},
+                                                                        "total": {"type": "string",
+                                                                                  "required": False,
+                                                                                  "empty": False,
+                                                                                  "nullable": True,
+                                                                                  "default": None}
+                                                                    }, 
+                                                                    "required": True,
+                                                                    "empty": False,
+                                                                    "dependencies": "format"}, 
+                                                            "arc": {"required": True, 
+                                                                    "empty": False, 
+                                                                    "dependencies": "format"}
                                                             }}})
 
     def __init__(self, path: str):
@@ -25,17 +73,33 @@ class ConfigParser():
 
             config = yaml.safe_load_all(f)
 
-        # Path to the template directory
-        ressources = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "templates"
-        )
+        try:
 
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(ressources))
+            if self.validator.validate(config):
 
-        # Load the header template
-        template = env.get_template("caller")
+                # Path to the template directory
+                ressources = os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)), "templates"
+                )
 
-        with open(f"{config['name']}.py", mode='w') as plugin:
+                env = jinja2.Environment(loader=jinja2.FileSystemLoader(ressources))
 
-            plugin.writelines(template.render(caller=config['name'], format=config["format"]))
+                # Load the header template
+                template = env.get_template("caller")
+
+                with open(f"{config['name']}.py", mode='w') as plugin:
+
+                    plugin.writelines(template.render(caller=config['name'], format=config["format"]))
+
+            else:
+
+                logger.error(self.validator.errors)
+
+                raise SystemExit("Error when parsing the config file.")
+        
+        except cerberus.DocumentError as e:
+
+            logger.error(e)
+
+            raise SystemExit(e)
 
